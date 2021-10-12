3c6371c00cde232f8e7c25be33b8122270ef8588
Hristo Jelev <jeleff@gmail.com>
2021-10-12 22:05:03 +0300

    comment prints

--- get_daily_temp.py
+++ get_daily_temp.py
@@ -32,19 +32,19 @@ def get_data():
 		last_record_date = datetime.strptime(last_line, '%Y-%m-%d %H:%M:%S')
 	for index, row in data.iterrows():
 		if isinstance(row[2], float):
-			print("NaN")
+			pass
 		else:
 			timestamp = "{}-{}-{} {}:00:00".format(date.today().year, date.today().month, row[0], row[1].zfill(2))
 			record_date = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
 			if record_date > last_record_date:
 				with open(csv_file, "a") as file_object:
 					new_line = "{},{}\n".format(timestamp,  row[2])
-					print(new_line)
+					#print(new_line)
 					file_object.write(new_line)
-			else:
-				print("Reading already exist")
+			#else:
+				#print("Reading already exist")
 
-	print(data)
+	#print(data)
 	
 	return 	
 		
