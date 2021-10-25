bb3791db48f2e29ea7691526bd00801ab81c26ea
Hristo Jelev <jeleff@gmail.com>
2021-10-25 21:47:36 +0300

    fix second part of month

--- get_daily_temp.py
+++ get_daily_temp.py
@@ -6,9 +6,6 @@ from datetime import date
 import csv
 from datetime import datetime
 
-
-
-# baseurl = "http://192.168.0.106:8000/site/sea_water_"
 baseurl = "https://www.stringmeteo.com/synop/sea_water.php?year="
 csv_file = "sea_water_temp.csv"
 	
@@ -16,35 +13,39 @@ def get_data():
 	url = baseurl + str(date.today().year) # Use this to parse stringmeteo.com site
 	tables = pd.read_html(url, encoding="utf8") # Returns list of all tables on page
 
-	data = tables[len(tables)-1]
+	data = tables[len(tables)-1] # Get the last table
 	# TBD: need to take care for Feb as its shorter
 	if date.today().day < 17: 
 		data = data[[0,1,3]]
+		index = [0,1,3]
+		row_to_extract = date.today().day * 3 - 1
 	else:
 		data = data[[5,6,8]]
-	
-	# 4,5,7
-	row_to_extract = date.today().day * 3 - 1
-	
+		index = [5,6,8]
+		row_to_extract = date.today().day * 3 - 1 - (16*3)
+
+
 	data = data.iloc[ row_to_extract : row_to_extract + 3 ]
+
+	# get the date of the last record in the csv
 	with open(csv_file, "r") as file_object:
 		last_line = file_object.readlines()[-1].split(",")[0].strip()
 		last_record_date = datetime.strptime(last_line, '%Y-%m-%d %H:%M:%S')
-	for index, row in data.iterrows():
-		if isinstance(row[3], float):
+		
+	for x, row in data.iterrows():
+		if isinstance(row[index[2]], float):
 			pass
 		else:
-			timestamp = "{}-{}-{} {}:00:00".format(date.today().year, date.today().month, row[0], row[1].zfill(2))
+			timestamp = "{}-{}-{} {}:00:00".format(date.today().year, date.today().month, row[index[0]], row[index[1]].zfill(2))
 			record_date = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
 			if record_date > last_record_date:
 				with open(csv_file, "a") as file_object:
-					new_line = "{},{}\n".format(timestamp,  row[3])
-					#print(new_line)
+					new_line = "{},{}\n".format(timestamp,  row[index[2]])
+					print(new_line)
 					file_object.write(new_line)
 			#else:
 				#print("Reading already exist")
 
-	#print(data)
 	
 	return 	
 		
