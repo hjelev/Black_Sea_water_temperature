1c00eaed1e6049fe049f27a427f6445e3b23b280
Hristo Jelev <jeleff@gmail.com>
2021-10-26 21:16:51 +0300

    separated code in functions

--- get_daily_temp.py
+++ get_daily_temp.py
@@ -8,16 +8,21 @@ from datetime import datetime
 
 baseurl = "https://www.stringmeteo.com/synop/sea_water.php?year="
 csv_file = "sea_water_temp.csv"
-	
-def get_data():
+
+def get_data_for_current_month(baseurl):
 	url = baseurl + str(date.today().year) # Use this to parse stringmeteo.com site
 	tables = pd.read_html(url, encoding="utf8") # Returns list of all tables on page
 	data = tables[len(tables)-1] # Get the last table
+	return data	
+
+def extract_today_data(data):
+	
 	# take care for Feb as its shorter
 	if date.today().month == 2:
 		month_middle = 16
 	else:
 		month_middle = 17
+		
 	if date.today().day < month_middle: 
 		data = data[[0,1,3]]
 		index = [0,1,3]
@@ -28,11 +33,16 @@ def get_data():
 		row_to_extract = date.today().day * 3 - 1 - ((month_middle -1) * 3)
 
 	data = data.iloc[ row_to_extract : row_to_extract + 3 ]
-
+	return data, index
+	
+def get_last_record_date(csv_file):
 	# get the date of the last record in the csv
 	with open(csv_file, "r") as file_object:
 		last_line = file_object.readlines()[-1].split(",")[0].strip()
 		last_record_date = datetime.strptime(last_line, '%Y-%m-%d %H:%M:%S')
+	return last_record_date
+	
+def save_new_data(data, index, last_record_date):
 	# Check and save daily temp. records
 	for x, row in data.iterrows():
 		# skip empty records
@@ -45,8 +55,12 @@ def get_data():
 				with open(csv_file, "a") as file_object:
 					new_line = "{},{}\n".format(timestamp,  row[index[2]])
 					file_object.write(new_line)
-	
-	return 	
+					
+def get_data():
+	data = get_data_for_current_month(baseurl)
+	data, index = extract_today_data(data)
+	last_record_date = get_last_record_date(csv_file)
+	save_new_data(data, index, last_record_date)	
 		
 if __name__ == '__main__':		   
 	get_data()
